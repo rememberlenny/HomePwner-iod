5680b3a721f7104046649a49dbeb89c7ec8f77a3
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-22 22:05:39 +0530

    Save the content of the cells and the store

--- HomePwner/ATRItemsViewController.m
+++ HomePwner/ATRItemsViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ATRItemsViewController.h"
+#import "ATRItemStore.h"
+#import "ATRItem.h"
 
 @interface ATRItemsViewController ()
 
@@ -18,6 +20,13 @@
 {
     // Call the superclass's designated initializer
     self = [super initWithStyle:UITableViewStylePlain];
+    
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[ATRItemStore sharedStore] createItem];
+        }
+    }
+    
     return self;
 }
 
@@ -34,6 +43,29 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Create an instnce of UITableViewCell, with default appearance
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                   reuseIdentifier:@"UITableViewCell"];
+
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    NSArray *items = [[ATRItemStore sharedStore] allItems];
+    NSArray *item = items[indexPath.row];
+
+    cell.textLabel.text = [item description];
+
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[ATRItemStore sharedStore] allItems] count];
 }
 
 @end
