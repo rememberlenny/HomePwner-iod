b9598ae577efaf2c6257e12d408b881f5a1594c2
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-23 14:13:08 +0530

    Create conditions for the header text

--- HomePwner/ATRDetailViewController.m
+++ HomePwner/ATRDetailViewController.m
@@ -58,14 +58,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setItem:(ATRItem *)item
+{
+    _item = item;
+    self.navigationItem.title = _item.itemName;
 }
-*/
 
 @end
--- HomePwner/ATRItemsViewController.m
+++ HomePwner/ATRItemsViewController.m
@@ -83,6 +83,8 @@
     self = [super initWithStyle:UITableViewStylePlain];
     
     if (self) {
+        UINavigationItem *navItem = self.navigationItem;
+        navItem.title = @"Homepwner";
     }
     
     return self;
