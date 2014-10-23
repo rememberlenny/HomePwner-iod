2eaa9b95f116a8270663ca726f43e3f91ae13812
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-23 15:18:07 +0530

    Set up the dictionary

---
+++ HomePwner/ATRImageStore.m
@@ -0,0 +1,69 @@
+//
+//  ATRImageStore.m
+//  HomePwner
+//
+//  Created by Leonard Bogdonoff on 10/23/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "ATRImageStore.h"
+
+@interface ATRImageStore()
+
+@property (nonatomic, strong) NSMutableDictionary *dictionary;
+
+@end
+
+@implementation ATRImageStore
+
++ (instancetype)sharedStore
+{
+    static ATRImageStore *sharedStore;
+    
+    if(!sharedStore) {
+        sharedStore = [[self alloc] initPrivate];
+    }
+    
+    return sharedStore;
+}
+
+// No one should call nit
+- (instancetype)init
+{
+    [NSException raise:@"Singleton"
+                format:@"Ise +[ATRImageStore sharedStore]"];
+    return nil;
+}
+
+//Secret designated initializer
+- (instancetype)initPrivate
+{
+    self = [super init];
+    
+    if (self) {
+        _dictionary = [[NSMutableDictionary alloc] init];
+    }
+    
+    return self;
+}
+
+- (void)setImage:(UIImage *)image forKey:(NSString *)key
+{
+    [self.dictionary setObject:image forKey:key];
+}
+
+- (UIImage *)imageForKey:(NSString *)key
+{
+    return [self.dictionary objectForKey:key];
+}
+
+- (void)deleteImageForKey:(NSString *)key
+{
+    if (!key) {
+        return;
+    }
+    
+    [self.dictionary removeObjectForKey:key];
+}
+
+@end
