66749804d606c153bb9b3996f12a5eeab73d78f9
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-24 01:30:48 -0400

    Assign properties for all the XIB outlets

--- HomePwner/ATRItemCell.h
+++ HomePwner/ATRItemCell.h
@@ -11,4 +11,9 @@
 
 @interface ATRItemCell : UITableViewCell
 
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnailView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *serialNumberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *valueLabel;
+
 @end
