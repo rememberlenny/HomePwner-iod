f77905fe98b859da1bb646283af12c19513ab36b
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-24 01:23:18 -0400

    Create item cell andremove header view

---
+++ HomePwner/ATRItemCell.h
@@ -0,0 +1,13 @@
+//
+//  ATRItemCell.h
+//  HomePwner
+//
+//  Created by Leonard Bogdonoff on 10/24/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATRItemCell : NSObject
+
+@end
---
+++ HomePwner/ATRItemCell.m
@@ -0,0 +1,13 @@
+//
+//  ATRItemCell.m
+//  HomePwner
+//
+//  Created by Leonard Bogdonoff on 10/24/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "ATRItemCell.h"
+
+@implementation ATRItemCell
+
+@end
